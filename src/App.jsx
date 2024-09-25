0f5756bcdf0015e354ec670a7ddf5cd5498c2b63
steve <stvreeno_3@yahoo.com>
2024-09-25 14:54:59 -0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AdminPanel from './components/AdminPanel'
+import LoginForm from './components/LoginForm'
+
+function App() {
+  const [content, setContent] = useState(0)
+  const isLoggedIn = true;
+
+  if (isLoggedIn){
+    setContent(<AdminPanel />)
+  } else {
+    setContent(<LoginForm />)
+  
+  }
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
